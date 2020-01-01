3e5c745147d7616fe46432b8bb97081da4be8334
yurii khiznichenko <yuriikhiznichenko@gmail.com>
2020-01-01 16:07:23 +0100

    committer mailto: link added

--- src/components/CommitSummary.tsx
+++ src/components/CommitSummary.tsx
@@ -9,12 +9,18 @@ interface ICommitSummaryProps {
 }
 
 const CommitSummary: React.FC<ICommitSummaryProps> = (props) => {
-    const date = moment(props.commit.commit.committer.date).calendar();
+    const details = props.commit.commit;
+    const date = moment(details.committer.date).calendar();
+    const mailToAuthorLink = `mailto:${details.committer.email}`;
+    const authorName = details.committer.name;
+
     return (
         <div className={s.wrap}>
             <div className={s.details}>
-                <div className={s.message}>{props.commit.commit.message}</div>
-                <div className={s.author}>by {props.commit.commit.committer.name} ({date})</div>
+                <div className={s.message}>{details.message}</div>
+                <div className={s.author}>
+                    by <a className={s.email} href={mailToAuthorLink}>{authorName}</a> ({date})
+                </div>
             </div>
             <a
                 className={s.link}
