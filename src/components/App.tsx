109b2408a8c031cbedada2db91ea571253618959
yurii khiznichenko <yuriikhiznichenko@gmail.com>
2020-01-01 10:34:06 +0100

    Webpack+React+Typescript added

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            App here !
+        </div>
+    );
+};
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        alias: { 'react-dom': '@hot-loader/react-dom'  },
+    },
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.min.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader'
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            { test: /\.html$/, use: 'html-loader' },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ]
+};
